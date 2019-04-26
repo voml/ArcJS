af6623646d01dc62a9b985e5e715444ea40986d2
Aster <galaster@foxmail.com>
2019-04-27 00:08:03 +0800

    💻 Support Atom String

--- projects/arc-reader/source/main.ts
+++ projects/arc-reader/source/main.ts
@@ -19,4 +19,41 @@ export class ObjectVisitor extends AbstractParseTreeVisitor<object> implements A
         //console.log("Program: " + element)
         return this.visit(ctx.getChild(0))
     }
+
+
+    /* Atom: String */
+    visitStringEmpty(ctx: ANTLR.StringEmptyContext) {
+        //console.log('String: EmptyString!')
+        return {
+            data: ''
+        }
+    }
+    visitStringLiteralSingle(ctx: ANTLR.StringLiteralSingleContext) {
+        const s = JSON.stringify(ctx.text).slice(2, -2)
+        //console.log('String: ' + s)
+        return {
+            data: s
+        }
+    }
+    visitStringLiteralBlock(ctx: ANTLR.StringLiteralBlockContext) {
+        const s = JSON.stringify(ctx.text).slice(4, -4)
+        //console.log('String: ' + s)
+        return {
+            data: s
+        }
+    }
+    visitStringEscapeSingle(ctx: ANTLR.StringEscapeSingleContext) {
+        const s = ctx.text.slice(1, -1)
+        //console.log('String: ' + s)
+        return {
+            data: s
+        }
+    }
+    visitStringEscapeBlock(ctx: ANTLR.StringEscapeBlockContext) {
+        const s = ctx.text.slice(3, -3)
+        //console.log('String: ' + s)
+        return {
+            data: s
+        }
+    }
 }
