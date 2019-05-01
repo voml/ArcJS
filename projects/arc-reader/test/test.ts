66c9ec4b9cfa40fd3031d04000131e74beecdf8c
Aster <galaster@foxmail.com>
2019-05-01 19:53:17 +0800

    💉 Add JSON and list tests

--- projects/arc-reader/test/test.ts
+++ projects/arc-reader/test/test.ts
@@ -2,7 +2,7 @@ import { TaskListVisitor } from '../source/task-list'
 import { ArcInputStream } from '../source/lib'
 import { readFileSync, writeFileSync } from 'fs'
 const Visitor = new TaskListVisitor()
-const files = ['dict', 'json']
+const files = ['list', 'dict', 'json', 'bugs']
 function testString(raw: string) {
     const tree = ArcInputStream(raw)
     const ans = Visitor.visitProgram(tree)
@@ -10,7 +10,7 @@ function testString(raw: string) {
 }
 function testFile(name: string) {
     const file = readFileSync(`${__dirname}/${name}.arc`)
-    const tree = ArcInputStream(`${ file.toString() }`)
+    const tree = ArcInputStream(`${file.toString()}`)
     const ans = Visitor.visitProgram(tree)
     writeFileSync(`${__dirname}/${name}.json`, JSON.stringify(ans, null, 4))
 }
@@ -18,9 +18,10 @@ function testFile(name: string) {
 
 /*
 testString(`
+a=[[]]
 `)
-    
-*/
+  */
+
 
 
 
